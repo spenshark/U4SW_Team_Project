4f3a944fc2dd32d5b835a67495f558565022a116
spenshark <xzx01299@naver.com>
2023-12-12 00:07:42 +0900

    code merge

--- animal_detect.py
+++ animal_detect.py
@@ -1,12 +1,39 @@
 import cv2
 import numpy as np
 
+# 사진 넣기
+# display resolution: FHD
+target_width = 1920	
+target_height = 1080
+
+image_name = input('enter your image name: ')
+
+image_path = './image/'+image_name+'.jpg'
+
+img = cv2.imread(image_path)
+
+if img is None:
+	print("\nEroor: File not found or unable to load.")
+	exit()
+
+original_height, original_width = img.shape[:2]
+
+resized_image = cv2.resize(img, (target_width, target_height))
+
+cv2.imshow("Resized Image", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+# 판단한 클래스 별로 OpenCV로 표현
 class_ids = []
 confidence_scores = []
 boxes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
+font = cv2.FONT_HERSHEY_PLAIN
 
 # load pre-trained yolo model or whatever from configuration and weight files
 # classify the image
